338207b643a204ba55c97f9e985655963304b2c8
Daniel12082 <137439716+Daniel12082@users.noreply.github.com>
2023-11-17 13:22:47 -0500

    feat: :construction: Tables

---
+++ Front-end/Src/Js/table.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('#dtBasicExample').DataTable({
+      "paging": false // false to disable pagination (or any other option)
+    });
+    $('.dataTables_length').addClass('bs-select');
+});
