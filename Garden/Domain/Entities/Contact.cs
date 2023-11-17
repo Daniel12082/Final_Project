56532959e88919c922dc06da1ad8b289d1be954c
Daniel12082 <137439716+Daniel12082@users.noreply.github.com>
2023-11-17 17:44:38 -0500

    feat: :building_construction:

---
+++ Garden/Domain/Entities/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Entities;
+
+public partial class Contact
+{
+    public int Id { get; set; }
+
+    public string ContactName { get; set; } = null!;
+
+    public string ContactLastName { get; set; } = null!;
+
+    public string ContactNumbrer { get; set; } = null!;
+
+    public string Fax { get; set; } = null!;
+
+    public virtual ICollection<Client> Clients { get; set; } = new List<Client>();
+
+    public virtual ICollection<TypeContact> TypeContacts { get; set; } = new List<TypeContact>();
+}
---
+++ Garden/Domain/Entities/Proveedor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Entities;
+
+public partial class Proveedor
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int? DentificationArd { get; set; }
+
+    public int? Cellphone { get; set; }
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
